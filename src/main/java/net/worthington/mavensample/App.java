13ba45c68a5ec0ca0416181aa8b855e6090540ff
Michael Worthington <michael.m.worthington@gmail.com>
2012-06-01 05:21:28 +0000

    adding maven code sample to assembla svn

---
+++ src/main/java/net/worthington/mavensample/App.java
@@ -0,0 +1,13 @@
+package net.worthington.mavensample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
